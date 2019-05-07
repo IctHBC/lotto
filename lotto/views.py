88ff5d042abce1a6c95bec11a643779e62f512de
Byung-Chan Han <hbc2017ict@gmail.com>
2019-05-07 20:17:34 +0900

    Deploy HeRoku

--- lotto/views.py
+++ lotto/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import random
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 def result(request):
-    return render(request, "result.html")
+    #input number
+    number_list=list()
+    for i in range(0,6):
+        number=request.GET['number'+str(i+1)]
+        #if number is null, redirect home.html
+        if number == '':
+            return redirect('home')
+        number_list.append(int(number))
+    #random number
+    rand_list = list()
+    for i in range(0,7):
+        rand = random.randrange(1, 46)
+        #when rand in read_list
+        while rand in rand_list:
+            rand = random.randrange(1, 46)
+            rand_list.append(rand)
+    #count same number
+    count=0
+    for i in range(0,6):
+        for j in range(0,7):
+            if number_list[i] == rand_list[j]:
+                count +=1
+    return render(request, "result.html",{'number_list':number_list,'rand_list':rand_list,'count':count})
+
+   
